d1a75984f93f3c26c55b86ae6821b9575023b34e
Ishtiaq uddin rahat <ishtiaqrahat74@gmail.com>
2026-06-28 22:26:49 +0600

    fix: force Google DNS for MongoDB SRV + update configs

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,9 @@
+const dns = require('dns');
 const mongoose = require('mongoose');
 const { MONGO_URI } = require('./env');
+
+// Force reliable DNS servers so MongoDB SRV lookup works on all networks
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
 const connectDatabase = () =>
   mongoose.connect(MONGO_URI).then(() => {
